2a510f16122a2f7c4364bb074d098964fc07affb
Steffen Schumacher <ssch@wheel.dk>
2019-09-12 09:41:35 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ciscoaplookup',
-    version="0.9.3",
+    version="0.9.4",
     author="Steffen Schumacher",
     author_email="ssch@wheel.dk",
     description="The Cisco Wireless LAN Compliance Lookup library",
