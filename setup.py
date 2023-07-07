fd4a076829910e6de9fd9997e6adf23f025ea24e
stsmr <stsmr@vestas.com>
2023-07-07 13:16:18 +0200

    Updated tabs in spreadsheet

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='ciscoaplookup',
-    version="0.12.0",
+    version="0.13.0",
     author="Steffen Schumacher",
     author_email="ssch@wheel.dk",
     description="The Cisco Wireless LAN Compliance Lookup library",
